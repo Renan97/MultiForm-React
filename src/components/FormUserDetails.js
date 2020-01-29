e8e335efcced979f2ef7136f4e252d0ee6778b8f
renan.ribeiro <rs.ls2408@gmail.com>
2020-01-29 13:39:02 -0300

    Add button to clear all fields

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,58 +1,64 @@
-import React, { Component } from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import AppBar from "material-ui/AppBar";
-import TextField from "material-ui/TextField";
-import RaisedButton from "material-ui/RaisedButton";
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component {
-  continue = e => {
-    e.preventDefault();
-    this.props.nextStep();
-  };
-  render() {
-    const { values, handleChange } = this.props;
+	continue = (e) => {
+		e.preventDefault();
+		this.props.nextStep();
+	};
+	render() {
+		const { values, handleChange } = this.props;
 
-    return (
-      <MuiThemeProvider>
-        <React.Fragment>
-          <AppBar title="Enter User Details"></AppBar>
-          <TextField
-            hintText="Enter your first name"
-            floatingLabelText="First name"
-            onChange={handleChange("firstName")}
-            defaultValue={values.firstName}
-          />
-          <br />
-          <TextField
-            hintText="Enter your last name"
-            floatingLabelText="Last name"
-            onChange={handleChange("lastName")}
-            defaultValue={values.lastName}
-          />
-          <br />
-          <TextField
-            hintText="Enter your email"
-            floatingLabelText="Email"
-            onChange={handleChange("email")}
-            defaultValue={values.email}
-          />
-          <br />
-          <RaisedButton
-            label="Continue"
-            primary={true}
-            style={StyleSheet.button}
-            onClick={this.continue}
-          />
-        </React.Fragment>
-      </MuiThemeProvider>
-    );
-  }
+		return (
+			<MuiThemeProvider>
+				<React.Fragment>
+					<AppBar title="Enter User Details" />
+					<TextField
+						hintText="Enter your first name"
+						floatingLabelText="First name"
+						onChange={handleChange('firstName')}
+						defaultValue={values.firstName}
+						value={values.firstName}
+					/>
+					<br />
+					<TextField
+						hintText="Enter your last name"
+						floatingLabelText="Last name"
+						onChange={handleChange('lastName')}
+						defaultValue={values.lastName}
+						value={values.lastName}
+					/>
+					<br />
+					<TextField
+						hintText="Enter your email"
+						floatingLabelText="Email"
+						onChange={handleChange('email')}
+						defaultValue={values.email}
+						value={values.email}
+					/>
+					{'  '}
+					<br />
+					<RaisedButton
+						label="Clean"
+						primary={false}
+						style={StyleSheet.button}
+						onClick={this.props.cleanFields(values)}
+					/>
+					{'      '}
+					<RaisedButton label="Continue" primary={true} style={StyleSheet.button} onClick={this.continue} />
+				</React.Fragment>
+			</MuiThemeProvider>
+		);
+	}
 }
 
 const styles = {
-  button: {
-    margin: 15
-  }
+	button: {
+		margin: 15
+	}
 };
 
 export default FormUserDetails;
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,72 +1,76 @@
-import React, { Component } from "react";
-import FormUserDetails from "./FormUserDetails";
-import FormPersonalDetails from "./FormPersonalDetails";
-import Confirm from "./Confirm";
-import Success from "./Success";
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
 
 export class UserForm extends Component {
-  state = {
-    step: 1,
-    firstName: "",
-    lastName: "",
-    email: "",
-    occupation: "",
-    city: "",
-    bio: ""
-  };
+	state = {
+		step: 1,
+		firstName: '',
+		lastName: '',
+		email: '',
+		occupation: '',
+		city: '',
+		bio: ''
+	};
 
-  //proceed to next step
-  nextStep = () => {
-    const { step } = this.state;
-    this.setState({ step: step + 1 });
-  };
+	//proceed to next step
+	nextStep = () => {
+		const { step } = this.state;
+		this.setState({ step: step + 1 });
+	};
 
-  //go back to previous step
-  prevStep = () => {
-    const { step } = this.state;
-    this.setState({ step: step - 1 });
-  };
+	//go back to previous step
+	prevStep = () => {
+		const { step } = this.state;
+		this.setState({ step: step - 1 });
+	};
 
-  //Handle fields changes
-  handleChange = input => e => {
-    this.setState({ [input]: e.target.value });
-  };
+	//Handle fields changes
+	handleChange = (input) => (e) => {
+		console.log(input, e.target.value);
+		this.setState({ [input]: e.target.value });
+	};
 
-  render() {
-    const { step } = this.state;
-    const { firstName, lastName, email, occupation, city, bio } = this.state;
-    const values = { firstName, lastName, email, occupation, city, bio };
+	cleanFields = (values) => (e) => {
+		Object.keys(values).forEach((value) => {
+			console.log(value);
+			this.setState({ [value]: '' });
+		});
+	};
 
-    switch (step) {
-      case 1:
-        return (
-          <FormUserDetails
-            nextStep={this.nextStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-      case 2:
-        return (
-          <FormPersonalDetails
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-      case 3:
-        return (
-          <Confirm
-            values={values}
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-          />
-        );
-      case 4:
-        return <Success />;
-    }
-  }
+	render() {
+		const { step } = this.state;
+		const { firstName, lastName, email, occupation, city, bio } = this.state;
+		const values = { firstName, lastName, email, occupation, city, bio };
+
+		switch (step) {
+			case 1:
+				return (
+					<FormUserDetails
+						nextStep={this.nextStep}
+						handleChange={this.handleChange}
+						values={values}
+						cleanFields={this.cleanFields}
+					/>
+				);
+			case 2:
+				return (
+					<FormPersonalDetails
+						nextStep={this.nextStep}
+						prevStep={this.prevStep}
+						handleChange={this.handleChange}
+						values={values}
+						cleanFields={this.cleanFields}
+					/>
+				);
+			case 3:
+				return <Confirm values={values} nextStep={this.nextStep} prevStep={this.prevStep} />;
+			case 4:
+				return <Success />;
+		}
+	}
 }
 
 export default UserForm;
